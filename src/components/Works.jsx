2a18ec00812bb840310f53821eec1217d08ef48d
ChrisM922 <112943995+ChrisM922@users.noreply.github.com>
2023-06-27 00:49:02 +0200

    Building componnents

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -7,6 +7,36 @@ import { SectionWrapper } from "../hoc";
 import { projects } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
 
+const ProjectCard = ({index, name, description, tags, image, source_code_link}) => {
+  return (
+    <motion.div
+    variants={fadeIn("up", "spring", index * 0.5, 1)}
+    >
+      <Tilt
+      options={{
+        max: 45,
+        sclae: 1,
+        speed: 450,
+      }}
+      className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
+      >
+        <div className="relative  w-full h-[230px]">
+        <img src={image} alt={name} className="w-full h-full object-cover rounded-2xl"/>
+        <div className="absolute inset-0 flex justify-end m-3 card-img-hover">
+          <div
+            onClick={() => window.open(source_code_link, "_blank")}
+            className="rounded-full cursor-pointer black-gradient w-10 h-10 flex justify-center items-center"
+          >
+            <img src={github} alt="github" className="w-1/2 h-1/2 object-contain" />
+          </div>
+        </div>
+        </div>
+
+      </Tilt>
+    </motion.div>
+  )
+}
+
 const Works = () => {
   return (
     <>
@@ -27,6 +57,15 @@ const Works = () => {
           and manage projects effectively.
         </motion.p>
       </div>
+      <div className="mt-20 flex flex-wrap gap-7">
+        {projects.map((project, index) => (
+          <ProjectCard key={`project-${index}`}
+          index={index}
+          {...project}
+          />
+        ))}
+      </div>
+
     </>
   );
 };
