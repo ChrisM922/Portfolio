169bccb3f22fa392aa89ff2d4e64765acf9b5f54
Christopher Mace <mace.christop@mm-bbs.de>
2023-07-02 16:40:13 +0200

    Small optimisations

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -39,7 +39,7 @@ const Contact = () => {
           from_name: form.name,
           to_name: "Christopher",
           from_email: form.email,
-          to_email: "christophermace7@gmail.com",
+          to_email: "contact@christopher-mace.com",
           message: form.message,
         },
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
