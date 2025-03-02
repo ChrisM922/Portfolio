9b4c7ae6dacb68613578e8edc5b679f58ae541cb
Chris Mace <christophermace7@gmail.com>
2025-03-03 00:25:23 +0100

    Add testing header to App component

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ const App = () => {
     <BrowserRouter>
       <div className="relative z-0 bg-primary">
         <div className="bg-hero-pattern bg-cover, bg-no-repeat bg-center">
+          <h1>TESTING</h1>
           <Navbar />
           <Hero />
         </div>
